a274bb7a0c124463e776293666afac51dcf5fcc3
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-08 21:21:22 +0300

    delete some lines

--- lib/views/edit_product_view.dart
+++ lib/views/edit_product_view.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:store_app/models/product_model.dart';
